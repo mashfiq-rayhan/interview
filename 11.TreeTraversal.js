8b2c42958cffd8eb42bba33c83c470e0e2a66387
mashfiq-rayhan <mashfiq.rayhan.ovi@gmail.com>
2026-02-08 12:33:54 +0600

    Squashed 'DSA/' content from commit c2147ad
    git-subtree-dir: DSA
    git-subtree-split: c2147ad6e450c3c44f1fbd5193125e11706b77c3

---
+++ 11.TreeTraversal.js
@@ -0,0 +1,131 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  #rInsert(value, currentNode = this.root) {
+    if (currentNode === null) return new Node(value);
+    if (value < currentNode.value) {
+      currentNode.left = this.#rInsert(value, currentNode.left);
+    } else if (value > currentNode.value) {
+      currentNode.right = this.#rInsert(value, currentNode.right);
+    }
+    return currentNode;
+  }
+
+  rInsert(value) {
+    this.root = this.#rInsert(value);
+  }
+
+  minValue(currentNode) {
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.value;
+  }
+
+  #deleteNode(value, currentNode) {
+    if (currentNode === null) return null;
+    if (value < currentNode.value) {
+      currentNode.left = this.#deleteNode(value, currentNode.left);
+    } else if (value > currentNode.value) {
+      currentNode.right = this.#deleteNode(value, currentNode.right);
+    } else {
+      if (currentNode.left === null && currentNode.right === null) {
+        return null;
+      } else if (currentNode.left === null) {
+        currentNode = currentNode.right;
+      } else if (currentNode.right === null) {
+        currentNode = currentNode.left;
+      } else {
+        let subTreeMin = this.minValue(currentNode.right);
+        currentNode.value = subTreeMin;
+        currentNode.right = this.#deleteNode(subTreeMin, currentNode.right);
+      }
+    }
+    return currentNode;
+  }
+
+  deleteNode(value) {
+    this.root = this.#deleteNode(value, this.root);
+  }
+
+  rContains(value, currentNode = this.root) {
+    if (currentNode === null) return false;
+    if (value === currentNode.value) return true;
+    if (value < currentNode.value) {
+      return this.rContains(value, currentNode.left);
+    } else {
+      return this.rContains(value, currentNode.right);
+    }
+  }
+
+  BFS() {
+    if (this.root === null) return [];
+    let queue = [this.root];
+    let results = [];
+    while (queue.length) {
+      const currentNode = queue.shift();
+      results.push(currentNode.value);
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+    return results;
+  }
+
+  DFSPreOrder() {
+    let results = [];
+    function traverse(currentNode) {
+      results.push(currentNode.value);
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(this.root);
+    return results;
+  }
+
+  DFSPostOrder() {
+    let results = [];
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+      results.push(currentNode.value);
+    }
+    traverse(this.root);
+    return results;
+  }
+
+  DFSInOrder() {
+    let results = [];
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      results.push(currentNode.value);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(this.root);
+    return results;
+  }
+}
+
+let myBST = new BST();
+
+const insert47 = myBST.rInsert(47);
+const insert21 = myBST.rInsert(21);
+const insert76 = myBST.rInsert(76);
+const insert18 = myBST.rInsert(18);
+const insert27 = myBST.rInsert(27);
+const insert52 = myBST.rInsert(52);
+const insert82 = myBST.rInsert(82);
+
+console.log(myBST.BFS());
+console.log(myBST.DFSPreOrder());
+console.log(myBST.DFSPostOrder());
+console.log(myBST.DFSInOrder());
---
+++ 12. BasicSorts.js
@@ -0,0 +1,52 @@
+// Bubble Sort
+function BubbleSort(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+const bblsrt = BubbleSort([4, 2, 6, 5, 1, 3]);
+console.log(bblsrt);
+
+// Selection Sort
+function SelectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) min = j;
+    }
+    if (i !== min) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+}
+
+const slctnsrt = SelectionSort([4, 2, 6, 5, 1, 3]);
+console.log(slctnsrt);
+
+// Insertion Sort
+function InsertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) {
+        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+      } else {
+        break;
+      }
+    }
+  }
+  return arr;
+}
+
+const insrtnsrt = InsertionSort([4, 2, 6, 5, 1, 3]);
+console.log(insrtnsrt);
